6af4eb1e52163512b7e959cc5a2ed3c4c8eabe5f
- <53183727+baptistegreve@users.noreply.github.com>
2023-03-27 09:27:20 +0200

    Fixed minor issue with conversations loading

--- components/Conversations.jsx
+++ components/Conversations.jsx
@@ -32,8 +32,9 @@ export default function Conversations({conversations, selectedConv, setSelectedC
         /** Save in state */
         setConversations(data);
         setSelectedConv(data[0]);
-        setConversationsLoading(false);
       }
+
+      setConversationsLoading(false);
     }
   }, [user]);
 
